a9df70706ca69f42d999c30c4001574f95ad4d22
agrawaldeepak386-lab <agrawaldeepak386@gmail.com>
2026-08-10 05:09:07 +0000

    updated cat_schema

--- data_engineering_project/src/cat_schema.sql
+++ data_engineering_project/src/cat_schema.sql
@@ -1,18 +1,6 @@
 
-create catalog if not exists dev_dep;
-create schema if not exists dev_dep.bronze;
-create schema if not exists dev_dep.silver;
-create schema if not exists dev_dep.gold;
-create volume if not exists dev_dep.bronze.raw;
-
-create catalog if not exists uat_dep;
-create schema if not exists uat_dep.bronze;
-create schema if not exists uat_dep.silver;
-create schema if not exists uat_dep.gold;
-create volume if not exists uat_dep.bronze.raw;
-
-create catalog if not exists prod_dep;
-create schema if not exists prod_dep.bronze;
-create schema if not exists prod_dep.silver;
-create schema if not exists prod_dep.gold;
-create volume if not exists prod_dep.bronze.raw;
+create catalog if not exists IDENTIFIER(COALESCE(NULLIF(:catalog, ''), 'dev_dep'));
+create schema if not exists IDENTIFIER(COALESCE(NULLIF(:catalog, ''), 'dev_dep')||'.bronze');
+create schema if not exists IDENTIFIER(COALESCE(NULLIF(:catalog, ''), 'dev_dep')||'.silver');
+create schema if not exists IDENTIFIER(COALESCE(NULLIF(:catalog, ''), 'dev_dep')||'.gold');
+create volume if not exists IDENTIFIER(COALESCE(NULLIF(:catalog, ''), 'dev_dep')||'.bronze.raw');
